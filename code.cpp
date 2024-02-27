7c12fab633c6df4956dc1d0874918d783d859276
GogipOrion <145480613+GogipOrion@users.noreply.github.com>
2024-02-27 23:27:32 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    int x, y, k, z = 1, i = 0, n;
+    cin >> x;
+    cin >> y;
+    cin >> k;
+
+    for (n = 0; k != i; n++)
+    {
+        z -= 1;
+        z += x;
+        if (z >= y)
+        {
+            z -= y;
+            i += 1;
+            n++;
+        }
+    }
+    for (n; k > z; n++)
+    {
+        z -= 1;
+        z += x;
+    }
+    cout << n << endl;
+    return 0;
 }
